aa699967dd629b0fd4036e71048df039eb693eb4
nbuechler <natebuechler@gmail.com>
2016-07-24 23:52:33 -0400

    start of restful service for getting parts of te raw_affect_corpus

--- app/corpus_storage/controllers.py
+++ app/corpus_storage/controllers.py
@@ -4,6 +4,8 @@ import requests
 
 from datetime import datetime
 
+from bson.json_util import loads, dumps
+
 def default():
     print 'Here'
     return 'Thanks controller, hello corpus_storage!'
@@ -22,11 +24,12 @@ This section is for databases
 from app import app
 from flask.ext.pymongo import PyMongo
 
+app.config['MONGOCORPUSRAW_DBNAME'] = 'affect-corpus'
+mongo_corpus = PyMongo(app, config_prefix='MONGOCORPUSRAW')
+
 app.config['MONGOCORPUSSTORAGE_DBNAME'] = 'affect-synopsis'
 mongo_corpus_storage = PyMongo(app, config_prefix='MONGOCORPUSSTORAGE')
 
-app.config['MONGOCORPUS_DBNAME'] = 'affect-corpus'
-mongo_corpus = PyMongo(app, config_prefix='MONGOCORPUS')
 
 '''
 The aim of corpus_storage python files are to store a list of synonyms for each order of the word.
@@ -45,3 +48,12 @@ and where order-1 is uses the word. This makes it easily referencable for Natura
 This object is stored in a collection, and is gernerated from the information stored in affect-corpus database.
 
 '''
+
+def get_mongo_corpus_collection_with_order(collection, order):
+    return dumps(mongo_corpus.db[collection + '-corpus-only-syn-unq-order-' + order].find())
+
+def get_flat_lists_for_collection_with_order(collection, order):
+    flat_lists = []
+    for doc in mongo_corpus.db[collection + '-corpus-only-syn-unq-order-' + order].find():
+        flat_lists += (doc['flat_list'])
+    return dumps(set(flat_lists))
--- app/corpus_storage/views.py
+++ app/corpus_storage/views.py
@@ -12,3 +12,15 @@ def default():
 @corpus_storage.route('/alt/')
 def controller_default():
     return controllers.default()
+
+@corpus_storage.route('/raw_corpus_collection/<collection>')
+def get_raw_corpus_collection(collection):
+    return 'Remember to specifiy an order to get back data for collection: ' + collection
+
+@corpus_storage.route('/raw_corpus_collection/<collection>/<order>/')
+def get_raw_corpus_collection_with_order(collection, order):
+    return controllers.get_mongo_corpus_collection_with_order(collection, order)
+
+@corpus_storage.route('/raw_corpus_collection/<collection>/<order>/flat_list')
+def get_flat_lists_for_collection_with_order(collection, order):
+    return controllers.get_flat_lists_for_collection_with_order(collection, order)
